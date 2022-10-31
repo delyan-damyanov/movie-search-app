f72beeeed1680092db048cf61ec782455b9b4497
Delyan Damyanov <damyanov.delyan@gmail.com>
2022-10-31 15:34:53 +0200

    feature: add movie-search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MovieSearchComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
