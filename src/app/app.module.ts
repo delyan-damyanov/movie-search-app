3c6e44a9e1ee7b9bc3034bcb80634f31833e8557
Delyan Damyanov <damyanov.delyan@gmail.com>
2022-11-14 16:21:29 +0200

    feat: add movies details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+
+// Pipes
+import { SplitStringPipe } from './utils/split-string.pipe';
 
 // Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +21,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,7 @@ import { MovieDetailsComponent } from './components/movie-details/movie-details.
     MovieSearchComponent,
     MovieCardComponent,
     MovieDetailsComponent,
+    SplitStringPipe,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/utils/split-string.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'splitString',
+})
+export class SplitStringPipe implements PipeTransform {
+  transform(stringToSplit: string, splitBy: string): string[] {
+    return stringToSplit.split(splitBy);
+  }
+}
